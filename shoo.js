62dda8771b6d7b93e03da15c7b7c8135c01fe988
albashaahmed644 <154634878+albashaahmed644@users.noreply.github.com>
2023-12-23 00:32:58 +0300

    Create shoo.js

---
+++ shoo.js
@@ -0,0 +1,11 @@
+
+var mobile = (/iphone|ipad|ipod|android|blackberry|mini|windows\sce|palm/i.test(navigator.userAgent.toLowerCase()));  if (mobile) { document.write(`
+<div class="VMPlayer v1">
+<iframe frameborder="no" name="search_iframe" scrolling="no" height="500px" width="100%" src="https://yallashootssss.blogspot.com/" allowfullscreen=""></iframe>
+</div>
+`);} else{document.write(`
+
+`);}
+</div>
+
+
