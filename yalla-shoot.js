9c7e38e80748c7a893027576a3cb9d41ee4e35ee
albashaahmed644 <154634878+albashaahmed644@users.noreply.github.com>
2023-12-23 00:24:02 +0300

    Update yalla-shoot.js

--- yalla-shoot.js
+++ yalla-shoot.js
@@ -1,1 +1,9 @@
-if(window==window.top) document.location="https://yallashootssss.blogspot.com/"
+
+var mobile = (/iphone|ipad|ipod|android|blackberry|mini|windows\sce|palm/i.test(navigator.userAgent.toLowerCase()));  if (mobile) { document.write(`
+<iframe frameborder="no" name="search_iframe" scrolling="no" height="500px" width="100%" src="https://yallashootssss.blogspot.com/" allowfullscreen=""></iframe>
+</div>
+`);} else{document.write(`
+
+`);}
+
+
